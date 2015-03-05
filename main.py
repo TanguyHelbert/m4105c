29f7a02d83cf465ca1c91072d0ced6c6a9a13006
Tanguy HELBERT <E134287P@U-INF-J-E114-20.iut-nantes.univ-nantes.prive>
2015-03-05 12:02:54 +0100

    ecriture dans la base de donnees

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-# pour faire une interface utiliser cherrypy
-
 import fiches.installation as ins
 import fiches.equipement as eq
 import BaseDeDonnees.dataBase as db
 items = ins.parse_json_installation("fiches_installations/fichesInstall.json")
 items2 = eq.parse_json_equipement("fiches_equipements/data.paysdelaloire.fr.json")
+items2csv = eq.parse_csv_equipement("fiches_equipements/equipements.csv")
 
-database = db.DataBase("equipement.db", "equipement", items2)
+database = db.DataBase("equipement.db", "equipement", items2csv)
 database.table()
 database.insert()
 liste = database.selectAll()
